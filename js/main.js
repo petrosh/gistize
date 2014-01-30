7e9d2197ce808322adf1cfa3c0a6e221cb9beed5
alessandro petrozzi <petrosh@petrosh.it>
2014-01-30 22:46:52 +0100

    sha check

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 $(function () {
     
+    // AUTH
     var github = new Github({
       token: "e693d2265de53b2d6a3bc8e1d881f9438e2a54d9",
       auth: "oauth"
     });
 
+    // REPO
     var repo = github.getRepo('petrosh', 'gistize');
-
-    repo.show(function(err, repo) {
-        $('#out').append(repo.git_commits_url);
+    
+    // COMMITS
+    repo.getCommits({ 'path': 'docs/'}, function(err, sha) {
+        $.each( sha , function ( index, value) {
+            $('#out').append( index, value.sha, ' - ' );
+        });
     });
     
     $('#submitfilename').click( function (e){
@@ -16,5 +21,19 @@ $(function () {
         var filename = $('#filename').val();
         repo.write('gh-pages', 'docs/' + filename + '.md', 'YOUR_NEW_CONTENTS', 'YOUR_COMMIT_MESSAGE', function(err) {});
     })
+
+    $('#submitsha').click( function (e){
+        e.preventDefault();
+        var sha = $('#sha').val();
+        
+        repo.getCommits({ 'sha': sha}, function(err, commit) {
+            $.each( commit , function ( index, value) {
+                $('#out').append( value, ' - ' );
+                console.log(value)
+            });
+        });
+        
+    })
+
 
 });
